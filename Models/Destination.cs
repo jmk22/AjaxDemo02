28cf3fe1dcd7008f2658d3fb2eda9ab26b4d6e06
Jill Kuchman <pillanoid@gmail.com>
2016-03-30 18:11:31 -0700

    Initial commit - set up for second AJAX lesson

---
+++ Models/Destination.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AjaxDemo.Models
+{
+    [Table("Destinations")]
+    public class Destination
+    {
+        [Key]
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int Id { get; set; }
+
+        public Destination(string city, string country, int id = 0)
+        {
+            City = city;
+            Country = country;
+            Id = id;
+        }
+        public Destination()
+        {
+
+        }
+    }
+}
